8f3fb2c99f3653960bcbaf3d520bbdf35b3e68fc
sumit sharma <ssumit20kr@gmail.com>
2026-06-14 22:52:14 +0530

    fix :cloud run issue fixing

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT} and bound to 0.0.0.0`);
 });
